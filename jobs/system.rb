f566faee7c954e91bde2508afb6b948f5b2a62e5
Manuel Stadelmann <mstadelmann@atex.com>
2015-10-28 22:05:16 +0100

    Syntax error in system.rb

--- jobs/system.rb
+++ jobs/system.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 get '/functions/halt' do
   stdin, stdout, stderr = Open3.popen3('sudo halt')
   if stdout.gets
-    send_event('systemText', { "text": "#{stdout.gets}" })
+    send_event('systemText', "{ 'text': '#{stdout.gets}' }")
   end
 
   stderr
@@ -13,7 +13,7 @@ end
 get '/functions/reboot' do
   stdin, stdout, stderr = Open3.popen3('sudo reboot')
   if stdout.gets
-    send_event('systemText', { "text": "#{stdout.gets}" })
+    send_event('systemText', "{ 'text': '#{stdout.gets}' }")
   end
 
   stderr
@@ -22,5 +22,5 @@ end
 SCHEDULER.every '10m', :first_in => 0 do |job|
 
   ips = %x[ hostname -I ]
-  send_event('systemText', { "text": "IP adresses: #{ips}" })
+  send_event('systemText', "{ 'text': 'IP adresses: #{ips}' }")
 end
